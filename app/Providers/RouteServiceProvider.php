92262808b5cee7fdb88308fd94d42e8d2836b10d
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-05-01 17:11:37 +0300

    [clean up] delete unneeded code.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -58,24 +58,6 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-        
-        $this->mapWebRoutes();
-        
-        //
-    }
-    
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
     }
     
     /**
