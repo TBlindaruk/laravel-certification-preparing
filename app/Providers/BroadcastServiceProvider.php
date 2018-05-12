16525cf61b62d1365fe1b1721b6d36b59f8645ee
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-05-12 16:20:56 +0300

    [broadcasting] Real-time chat.  - basic configuration for broadcasting.

---
+++ app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast;
+
+/**
+ * Class BroadcastServiceProvider
+ * @package App\Providers
+ */
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Broadcast::routes();
+        
+        require base_path('routes/channels.php');
+    }
+}
